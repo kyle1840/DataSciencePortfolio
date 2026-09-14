c51ec6c8683ac69d9f68c481a04309e7c5863417
kyle1840 <152218821+kyle1840@users.noreply.github.com>
2026-09-14 14:58:41 -0700

    Delete DSProfession/Projects/Greeting-Project/hello.py

--- DSProfession/Projects/Greeting-Project/hello.py
+++
@@ -1,8 +0,0 @@
-# My first Python project in GitHub
-
-def greet_user():
-  name = input("Enter your name: ")
-  print(f"Hello, {name}! Welcome to Data Science.")
-
-if__name__=="__main__":
-greet_user()
