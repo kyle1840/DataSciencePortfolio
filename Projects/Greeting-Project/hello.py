e905134a7b1f1cfcb93df73d12ecdcbc0ce75fec
kyle1840 <152218821+kyle1840@users.noreply.github.com>
2026-09-14 15:09:52 -0700

    Update hello.py
    Added a question about the user's interest in Data Science.

--- Projects/Greeting-Project/hello.py
+++ Projects/Greeting-Project/hello.py
@@ -2,7 +2,10 @@
 
 def greet_user():
   name = input("Enter your name: ")
+  interest = input("What area of Data Science interests you? ")
+  
   print(f"Hello, {name}! Welcome to Data Science.")
+  print(f"It's great that you're interested in {interest}.")
 
 if __name__ == "__main__":
   greet_user()
